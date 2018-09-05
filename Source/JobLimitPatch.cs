3771938758e8cea28692374f1051dedfd8fa248e
NestOfMoon <34845466+NestOfMoon@users.noreply.github.com>
2018-09-05 11:31:06 +0300

    b19, proMode
    up for b19. Nothing special. Just had to recompile.
    proMode - allows to remove logical limmiter on mod values.

--- Source/JobLimitPatch.cs
+++ Source/JobLimitPatch.cs
@@ -1,4 +1,4 @@
-﻿using Harmony;
+using Harmony;
 using HugsLib;
 using HugsLib.Settings;
 using System;
@@ -23,6 +23,8 @@ namespace NestOfMoon.JobsLimitTweak.Standalone
 
         private SettingHandle<int> jobsLimitHandle;
         private SettingHandle<int> ticksLimitHandle;
+        private SettingHandle<bool> proModeHandle;
+
         protected override bool HarmonyAutoPatch => false;
         private HarmonyInstance harmony;
 
@@ -38,9 +40,11 @@ namespace NestOfMoon.JobsLimitTweak.Standalone
 
         public override void DefsLoaded()
         {
-            jobsLimitHandle = Settings.GetHandle<int>("JobsLimitField", "JobsLimit", "How many jobs pawn can start per TicksLimit. Recomended is 40, recomended maximum is 127, the theoretical maximum is 2147483647", 40);
-            ticksLimitHandle = Settings.GetHandle<int>("TicksLimitField", "TicksLimit", "It's just here. Minimum is 10, recomended is 10, recomended maximum is 127, the theoretical maximum is 2147483647", 10);
+            jobsLimitHandle = Settings.GetHandle<int>("JobsLimitField", "JobsLimit", "How many jobs a Pawn can start per TicksLimit. Recomended is 40, minimum is 5, recomended maximum is 127, the theoretical maximum is 2147483647", 40);
+            ticksLimitHandle = Settings.GetHandle<int>("TicksLimitField", "TicksLimit", "It's just here. This is how many ticks will pass before resetting JobsLimit count. Minimum is 5, recomended is 10, recomended maximum is 127, the theoretical maximum is 2147483647", 10);
+            proModeHandle = Settings.GetHandle<bool>("ProModeField", "ProMode", "Removing logical limitters on all fields. Use with caution.", false);
             
+
             Patch();
         }
 
@@ -48,9 +52,20 @@ namespace NestOfMoon.JobsLimitTweak.Standalone
 
         public void Patch()
         {
-            jobLim = jobsLimitHandle.Value;
+
+            jobLim =  jobsLimitHandle.Value;
             tickLim = ticksLimitHandle.Value;
-            if(harmony == null)
+
+            // If not in proMode. 
+            if (!proModeHandle.Value)
+            {
+                // System.Math is being used to limit the lower bound.
+                jobLim = Math.Max(jobLim, 5);
+                tickLim = Math.Max(tickLim, 5);
+            }
+
+
+            if (harmony == null)
                 harmony = HarmonyInstance.Create("com.github.harmony.rimworld.nestofmoon.jobslimittweak");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
@@ -63,46 +78,50 @@ namespace NestOfMoon.JobsLimitTweak.Standalone
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             var ils = instructions.ToArray();
+            
 
             // Ticks.
-            // Original IL is at number 22, and instruction is Ldc.i4.s 10
-            if(!ils[22].ModifyLdci4(Mod.tickLim))
-                Mod.log.Warning("Mod is outdated. If this is the only 'warn' then mod can be used in default way. Failed to transpile Pawn_JobTracker, FinalizeTick, at il 22, TicksLimit");
+            // Original IL is at index 22, and instruction is 'Ldc.i4.s 10'
+            if(ils[22].ModifyLdci4(Mod.tickLim) == 0)
+                Mod.log.Warning("Mod is outdated. If this is the only 'warning' then mod can be used. Details: Failed to transpile Pawn_JobTracker, FinalizeTick, at il 22, TicksLimit");
 
-           // Jobs.
-           // Original IL is at number 49, and instruction is Ldc.i4.s 10
-           if(!ils[49].ModifyLdci4(Mod.jobLim))
-            Mod.log.Warning("Mod is outdated. Failed to transpile Pawn_JobTracker, FinalizeTick, at il 49, JobsLimit");
+            // Jobs.
+            // Original IL is at index 49, and instruction is 'Ldc.i4.s 10'
+            if (ils[49].ModifyLdci4(Mod.jobLim) == 0)
+                Mod.log.Warning("Mod is outdated. Details: Failed to transpile Pawn_JobTracker, FinalizeTick, at il 49, JobsLimit");
 
             return ils;
         }
 
         // Helper method.
-        public static bool ModifyLdci4(this CodeInstruction il, int val)
+        public static int ModifyLdci4(this CodeInstruction il, int val)
         {
-            if (il.opcode == OpCodes.Ldc_I4_S || il.opcode == OpCodes.Ldc_I4)
+            // Report null reference.
+            if (il == null) return 2;
+            
+            // Check if opcodes types does match to SByte or Int32.
+            if (il.opcode == OpCodes.Ldc_I4_S || il.opcode == OpCodes.Ldc_I4) 
             {
-
-                if (val >= 10)
+                // Check if val is different, otherwise do nothing.
+                if (val != il.operand.ChangeType<int>())
                 {
-                    if (val != il.operand.ChangeType<int>())
+                    // If val is in range of SignByte.
+                    if (val <= sbyte.MaxValue)
                     {
-                        if (val <= sbyte.MaxValue)
-                        {
-                            if (il.opcode == OpCodes.Ldc_I4)
-                                il.opcode = OpCodes.Ldc_I4_S;
-                            il.operand = (SByte)val;
-                        }
-                        else
-                        {
-                            il.opcode = OpCodes.Ldc_I4;
-                            il.operand = (int)val;
-                        }
+                        il.opcode = OpCodes.Ldc_I4_S;
+                        il.operand = (SByte)val;
+                    }
+                    // Otherwise write it as Int32
+                    else
+                    {
+                        il.opcode = OpCodes.Ldc_I4;
+                        il.operand = (Int32)val;
                     }
                 }
-                return true;
+                
+                return 0;
             }
-            else return false;
+            else return 1;
         }
 
     }
